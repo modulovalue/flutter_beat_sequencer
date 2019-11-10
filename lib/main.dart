ba3f0a04a6c22aec076cf9e815f81db686e7c2ad
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-11-10 20:55:41 +0100

    Fix title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tonequencer Flutter Web',
+      title: 'Flutter Web Beat Sequencer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
