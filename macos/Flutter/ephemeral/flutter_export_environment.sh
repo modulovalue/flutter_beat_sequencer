41a3bdafc3a578717f982f6345cea2d54cdd93df
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-11-10 20:52:33 +0100

    -

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/valauskasmodestas/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/valauskasmodestas/Desktop/my github/Unfinished Projects/tonequencer_flutterweb"
-export "FLUTTER_TARGET=/Users/valauskasmodestas/Desktop/my github/Unfinished Projects/tonequencer_flutterweb/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/valauskasmodestas/Desktop/my github/Finished Projects/flutter_beat_sequencer"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_FRAMEWORK_DIR=/Users/valauskasmodestas/Desktop/flutter/bin/cache/artifacts/engine/darwin-x64"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
